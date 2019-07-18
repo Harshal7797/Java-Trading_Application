8af7e39f701ac497846bc77c45a454dface1c9f1
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-07-18 15:25:17 +0000

    MarketDataDAO Completed

--- src/main/java/ca/jrvs/apps/dao/MarketDataDao.java
+++ src/main/java/ca/jrvs/apps/dao/MarketDataDao.java
@@ -1,6 +1,7 @@
 package ca.jrvs.apps.dao;
 
 import ca.jrvs.apps.model.domain.IexQuote;
+import ca.jrvs.apps.util.JsonUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -12,7 +13,9 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,77 +23,91 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+
+@Repository
 public class MarketDataDao {
-//pk_e493228396be42e197a33006d25246b1
+
     private Logger logger = LoggerFactory.getLogger(MarketDataDao.class);
-    public final String BATCH_QUOTE_URL;
 
+    private final String BATCH_QUOTE_URL;
     private HttpClientConnectionManager httpClientConnectionManager;
-    public MarketDataDao(HttpClientConnectionManager httpClientConnectionManager) {
-        this.httpClientConnectionManager= httpClientConnectionManager;
-        BATCH_QUOTE_URL="https://cloud.iexapis.com/stable/tops?token=pk_3872a146e4c945f4b922051bb8a11859&symbols=aapl" ;
 
+    @Autowired
+    public MarketDataDao(HttpClientConnectionManager httpClientConnectionManager) {
+        this.httpClientConnectionManager = httpClientConnectionManager;
+        BATCH_QUOTE_URL = "https://cloud.iexapis.com/stable/stock/market/batch?symbols=aapl&types=quote&token=pk_3872a146e4c945f4b922051bb8a11859";
     }
 
-    public List<IexQuote> findIexQuoteByTicker(List<String> tickerList){
-    //convert list into comma Separated String
+    /**
+     * @throws DataRetrievalFailureException if unable to get http response
+     */
+    public List<IexQuote> findIexQuoteByTicker(List<String> tickerList) {
+        //convert list into comma Separated String
         String tickers = StringUtils.join(tickerList, ',');
         String uri = String.format(BATCH_QUOTE_URL, tickers);
-        logger.info("Get URI" + uri);
+        logger.info("Get URI:" + uri);
         //Get Http response body in string
         String response = executeHttpGet(uri);
-        System.out.println(response);
+        JSONObject IexQuotesJson = new JSONObject(response);
+        if (IexQuotesJson.length() == 0) {
+                    }
         //Iex will skip invalid symbols/ticker..we need to check it
-        JSONObject IexQuoteJson = new JSONObject(response);
-        System.out.println(IexQuoteJson);
-//        if (IexQuoteJson.length() != tickerList.size()){
-//            throw new IllegalArgumentException("Invalid ticker/symlbol");
-//      }
+        if (IexQuotesJson.length() != tickerList.size()) {
+            throw new IllegalArgumentException("Invalid ticker/symbol");
+        }
 
-        //Unmarshal JSOn object
+        //Unmarshal JSON object (Try diffrent approach)
         List<IexQuote> iexQuotes = new ArrayList<>();
-
+        IexQuotesJson.keys().forEachRemaining(ticker -> {
+              try {
+                  String quoteStr = ((JSONObject) IexQuotesJson.get(ticker)).get("quote").toString();
+                  IexQuote iexQuote = JsonUtil.toObjectFromJson(quoteStr, IexQuote.class);
+                  iexQuotes.add(iexQuote);
+              } catch (IOException e) {
+                  throw new DataRetrievalFailureException(
+                          "Unable parse response:" + IexQuotesJson.get(ticker), e);
+              }
+        });
         return iexQuotes;
     }
 
-    private String executeHttpGet(String url) {
-        try (CloseableHttpClient httpClient = getHttpClient()){
-            HttpGet httpGet = new HttpGet(url);
-            try (CloseableHttpResponse response = httpClient.execute(httpGet)){
-                switch (response.getStatusLine().getStatusCode()){
-                    case 200:
-                        //EntityUtils toString will also close inputStream in Entity
-                        String body = EntityUtils.toString(response.getEntity());
-                        return Optional.ofNullable(body.replaceAll("\\[", "").replaceAll("\\]","")).orElseThrow(
-                                () -> new IOException("Unexpected empty http response body"));
-                    case 404:
-                        throw new ResourceNotFoundException("Not found");
-                    default:
-                        throw new DataRetrievalFailureException(
-                                "Unexpected status:" + response.getStatusLine().getStatusCode());
-                }
-            }
-        } catch (IOException | ResourceNotFoundException e) {
-            throw new DataRetrievalFailureException("Unable Http execution error", e);
-        }
-    }
-
-    public IexQuote findIexQuoteByTicker(String ticker){
-        List<IexQuote> quotes =findIexQuoteByTicker(Arrays.asList(ticker));
-        if(quotes == null || quotes.size() != 1){
+    public IexQuote findIexQuoteByTicker(String ticker) {
+        List<IexQuote> quotes = findIexQuoteByTicker(Arrays.asList(ticker));
+        if (quotes == null || quotes.size() != 1) {
             throw new DataRetrievalFailureException("Unable to get data");
         }
         return quotes.get(0);
     }
 
-    private CloseableHttpClient getHttpClient(){
+    protected String executeHttpGet(String url) {
+        try (CloseableHttpClient httpClient = getHttpClient()) {
+            HttpGet httpGet = new HttpGet(url);
+            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                switch (response.getStatusLine().getStatusCode()) {
+                    case 200:
+                        //EntityUtils toString will also close inputStream in Entity
+                        String body = EntityUtils.toString(response.getEntity());
+                        return Optional.ofNullable(body).orElseThrow(
+                                () -> new IOException("Unexpected empty http response body"));
+                    case 404:
+                        throw new Exception("Not found");
+                    default:
+                        throw new DataRetrievalFailureException(
+                                "Unexpected status:" + response.getStatusLine().getStatusCode());
+                }
+            }
+        } catch (Exception e) {
+            throw new DataRetrievalFailureException("Unable Http execution error", e);
+        }
+    }
+
+    private CloseableHttpClient getHttpClient() {
         return HttpClients.custom()
                 .setConnectionManager(httpClientConnectionManager)
-                //prevent connectionManager shutdown when calling httpClient.clase()
+                //prevent connectionManager shutdown when calling httpClient.close()
                 .setConnectionManagerShared(true)
                 .build();
     }
-
     public static void main(String[] args) {
         PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
         cm.setMaxTotal(100);
@@ -99,7 +116,7 @@ public class MarketDataDao {
         testi.add("AAPL");
         testi.add("MSFT");
         MarketDataDao test =new MarketDataDao(cm);
-       test.findIexQuoteByTicker("AAPL");
-       // test.findIexQuoteByTicker(testi);
+        test.findIexQuoteByTicker("aapl");
+        // test.findIexQuoteByTicker(testi);
     }
 }
