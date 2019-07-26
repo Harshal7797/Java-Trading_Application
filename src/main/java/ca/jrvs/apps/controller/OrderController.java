afd91ef4e8a447209cc44f92d9c491ac42a7125e
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-07-26 19:33:17 +0000

    Trader Controller Completed

--- src/main/java/ca/jrvs/apps/controller/OrderController.java
+++ src/main/java/ca/jrvs/apps/controller/OrderController.java
@@ -1,7 +1,34 @@
 package ca.jrvs.apps.controller;
 
+import ca.jrvs.apps.model.domain.SequrityOrder;
+import ca.jrvs.apps.model.dto.MarketOrderDto;
+import ca.jrvs.apps.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
+@RequestMapping("/order")
 public class OrderController {
+    private OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService){
+        this.orderService = orderService;
+    }
+
+    @PutMapping(path = "/marketOrder")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public SequrityOrder makeMarketOrder(MarketOrderDto marketOrderDto){
+     try{
+         return orderService.executeMarketOrder(marketOrderDto);
+     }catch (Exception e){
+         throw ResponseExceptionUtil.getResponseStatusException(e);
+     }
+    }
 }
