c4b60f7c72bc41db2605d98ec0f11f3ef095a954
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-07-27 18:52:10 +0000

    DashBoard Completed

--- src/main/java/ca/jrvs/apps/controller/DashboardController.java
+++ src/main/java/ca/jrvs/apps/controller/DashboardController.java
@@ -1,9 +1,43 @@
 package ca.jrvs.apps.controller;
 
+import ca.jrvs.apps.model.view.PortfolioView;
+import ca.jrvs.apps.model.view.TraderAccountView;
+import ca.jrvs.apps.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
+
+    private DashboardService dashboardService;
+
+    @Autowired
+    public DashboardController(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
+
+    @GetMapping(path = "/profile/traderId{traderId}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public TraderAccountView getview(@PathVariable Integer traderId){
+        try{
+            return dashboardService.getTraderAccount(traderId);
+        }catch (Exception e){
+            throw ResponseExceptionUtil.getResponseStatusException(e);
+        }
+    }
+
+    @GetMapping(path = "/portfolio/traderId/{traderId}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public PortfolioView getPortfolioView(@PathVariable Integer traderId){
+        try{
+            return dashboardService.getProfileViewByTraderId(traderId);
+        }catch (Exception e){
+            throw ResponseExceptionUtil.getResponseStatusException(e);
+        }
+    }
 }
--- src/main/java/ca/jrvs/apps/service/DashboardService.java
+++ src/main/java/ca/jrvs/apps/service/DashboardService.java
@@ -1,4 +1,98 @@
 package ca.jrvs.apps.service;
 
+import ca.jrvs.apps.dao.*;
+import ca.jrvs.apps.model.domain.Account;
+import ca.jrvs.apps.model.domain.Position;
+import ca.jrvs.apps.model.domain.Trader;
+import ca.jrvs.apps.model.view.PortfolioView;
+import ca.jrvs.apps.model.view.SecurityRow;
+import ca.jrvs.apps.model.view.TraderAccountView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@Transactional
 public class DashboardService {
+
+    private TraderDao traderDao;
+    private PositionDao positionDao;
+    private AccountDao accountDao;
+    private QuoteDao quoteDao;
+
+    @Autowired
+    public DashboardService(TraderDao traderDao, PositionDao positionDao, AccountDao accountDao, QuoteDao quoteDao) {
+        this.traderDao = traderDao;
+        this.positionDao = positionDao;
+        this.accountDao = accountDao;
+        this.quoteDao = quoteDao;
+    }
+    /**
+     * Create and retrun a traderAccountView by trader ID
+     * -get trader account by id
+     * -get trader info by id
+     * -create and return a traderAccountView
+     *
+     *@param traderId trader ID
+     *@return traderAccountView
+     *@throws ca.jrvs.apps.dao.ResourceNotFoundException if ticker is not found from IEX
+     *@throws org.springframework.dao.DataAccessException if unable to retrieve data
+     *@throws IllegalArgumentException for invalid input
+     */
+    public TraderAccountView getTraderAccount(Integer traderId){
+        if(traderId ==null){
+            throw new IllegalArgumentException("TraderID cannot be null");
+        }
+        TraderAccountView traderAccountView = new TraderAccountView();
+        Account account = accountDao.findById(traderId);
+        Trader trader = traderDao.findById(traderId);
+        try{
+            traderAccountView.setAccount(account);
+            traderAccountView.setTrader(trader);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException("Could not create traderAccountView ",e);
+        }
+
+        return traderAccountView;
+    }
+
+    /**
+     * create and retrun portfolioView by trader ID
+     * -get account by trader id
+     * -get position by account id
+     * -create and return portfolioView
+     * @param traderId trader ID
+     * @return portfolioView
+     * @throws ca.jrvs.apps.dao.ResourceNotFoundException if ticker is not found from IEX
+     * @throws org.springframework.dao.DataAccessException if unable to retrieve data
+     * @throws IllegalArgumentException for invalid input
+     */
+    public PortfolioView getProfileViewByTraderId(Integer traderId) {
+        if(traderId ==null){
+            throw new IllegalArgumentException("TraderID cannot be null");
+        }
+        PortfolioView portfolioView = new PortfolioView();
+        Account account = accountDao.findById(traderId);
+        List<Position> positions = positionDao.findById(account.getId());
+        SecurityRow securityRow = new SecurityRow();
+
+        try{
+            positions.forEach(x-> {
+                        securityRow.setPosition(x);
+                        securityRow.setTicker(x.getTicker());
+                        securityRow.setQuote(quoteDao.findById(x.getTicker()));
+                        portfolioView.setSecurityRows(Collections.singletonList(securityRow));
+            }
+            );
+        }catch (DataAccessException e){
+            throw new ResourceNotFoundException("Resource not Found");
+        }
+
+        return  portfolioView;
+    }
 }
