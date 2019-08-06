95838638bf0b27fce54df4a18fb3cb3d7ca0d996
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-08-06 14:15:50 +0000

    Completed

--- src/test/java/ca/jrvs/apps/trading/service/OrderServiceTest.java
+++ src/test/java/ca/jrvs/apps/trading/service/OrderServiceTest.java
@@ -1,12 +1,83 @@
 package ca.jrvs.apps.trading.service;
 
+import ca.jrvs.apps.trading.dao.AccountDao;
+import ca.jrvs.apps.trading.dao.PositionDao;
+import ca.jrvs.apps.trading.dao.QuoteDao;
+import ca.jrvs.apps.trading.dao.SecurityOrderDao;
+import ca.jrvs.apps.trading.model.domain.Account;
+import ca.jrvs.apps.trading.model.domain.OrderStatus;
+import ca.jrvs.apps.trading.model.domain.Quote;
+import ca.jrvs.apps.trading.model.domain.SequrityOrder;
+import ca.jrvs.apps.trading.model.dto.MarketOrderDto;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class OrderServiceTest {
+    //capture parameter when calling securityOrderDao.save
+    @Captor
+    ArgumentCaptor<SequrityOrder> captorSecurityOrder;
+
+    //mock all dependencies
+    @Mock
+    private AccountDao accountDao;
+    @Mock
+    private SecurityOrderDao securityOrderDao;
+    @Mock
+    private QuoteDao quoteDao;
+    @Mock
+    private PositionDao positionDao;
+
+    //injecting mocked dependencies to the testing class via constructor
+    @InjectMocks
+    private OrderService orderService;
+
+    //setup test data
+    private MarketOrderDto orderDto;
 
     @Before
-    public void setUp() throws Exception {
+    public void setup() {
+        orderDto = new MarketOrderDto();
+        orderDto.setAccountId(1);
+        orderDto.setSize(1);
+        orderDto.setTicker("AAPL");
     }
+
+    @Test
+    public void executeMarketOrderHappyPath() {
+        when(quoteDao.existsById(orderDto.getTicker())).thenReturn(true);
+        when(accountDao.existsById(orderDto.getAccountId())).thenReturn(true);
+        Quote quote = new Quote();
+        quote.setAskSize(10);
+        quote.setAskPrice(100.00);
+        when(quoteDao.findById(orderDto.getTicker())).thenReturn(quote);
+
+        Account account = new Account();
+        account.setAmount(100.00);
+        account.setId(orderDto.getAccountId());
+        //accountDao.findByIdForUpdate equivalents accountDao.findById
+        when(accountDao.findByIdForUpdate(orderDto.getAccountId())).thenReturn(account);
+
+        orderService.executeMarketOrder(orderDto);
+        verify(securityOrderDao).save(captorSecurityOrder.capture());
+        SequrityOrder captorOrder = captorSecurityOrder.getValue();
+        assertEquals(OrderStatus.FILLED, captorOrder.getStatus());
+    }
+
+    @Test
+    public void executeMarketOrderSadPath() {
+        //write more tests
+
+    }
+
 }
